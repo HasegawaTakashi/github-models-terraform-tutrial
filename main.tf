98a68c70c4a40a29562955f3b0a8665aff5d51a4
HasegawaTakashi-Indigo <takashi.hasegawa@indigo.co.jp>
2025-09-19 19:52:33 +0900

    Test: static analysis check

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+resource "docker_image" "nginx1" {
+  name = "nginx:latest"
+  keep_locally = true
 }
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "tutorial"
+  name         = "x"
   ports {
-    internal = 80
-    external = 8000
+      internal = 80
+    external = 8080
   }
+  labels = {}
 }
